36880a8b47fb7091cc8049efac5883a8a800b2e8
Greg Doud <gpdoud@gmail.com>
2023-08-24 16:34:03 -0400

    Finished CustomersController

--- CustomersControllerProject/CustomersController.cs
+++ CustomersControllerProject/CustomersController.cs
@@ -12,6 +12,55 @@ public class CustomersController {
         this.sqlConnection = sqlConnection;
     }
 
+    public void DeleteCustomer(int id) {
+        var sql = " DELETE Customers " +
+                    " Where Id = @Id; ";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", id);
+        var rowsAffected = cmd.ExecuteNonQuery();
+        if(rowsAffected != 1) {
+            throw new Exception($"Delete failed! RA is {rowsAffected}");
+        }
+    }
+
+    public void UpdateCustomer(Customer customer) {
+        var sql = " UPDATE Customers Set " +
+                    " Name = @Name, " +
+                    " City = @City, " +
+                    " State = @State, " +
+                    " Sales = @Sales, " +
+                    " Active = @Active " +
+                    " Where Id = @Id; ";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", customer.Id);
+        cmd.Parameters.AddWithValue("@Name", customer.Name);
+        cmd.Parameters.AddWithValue("@City", customer.City);
+        cmd.Parameters.AddWithValue("@State", customer.State);
+        cmd.Parameters.AddWithValue("@Sales", customer.Sales);
+        cmd.Parameters.AddWithValue("@Active", customer.Active);
+        var rowsAffected = cmd.ExecuteNonQuery();
+        if(rowsAffected != 1) {
+            throw new Exception($"Update failed! RA is {rowsAffected}");
+        }
+    }
+
+    public void InsertCustomer(Customer customer) {
+        var sql = " INSERT Customers " +
+                    " (Name, City, State, Sales, Active) VALUES " +
+                    " (@Name, @City, @State, @Sales, @Active) ";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        //cmd.Parameters.AddWithValue("@Id", 0);
+        cmd.Parameters.AddWithValue("@Name", customer.Name);
+        cmd.Parameters.AddWithValue("@City", customer.City);
+        cmd.Parameters.AddWithValue("@State", customer.State);
+        cmd.Parameters.AddWithValue("@Sales", customer.Sales);
+        cmd.Parameters.AddWithValue("@Active", customer.Active);
+        var rowsAffected = cmd.ExecuteNonQuery();
+        if(rowsAffected != 1) {
+            throw new Exception($"Insert failed! RA is {rowsAffected}");
+        }
+    }
+
     public Customer? GetCustomerById(int id) {
         if(id <= 0) {
             throw new ArgumentException("'id' must be greater than zero");
@@ -39,6 +88,28 @@ public class CustomersController {
     public List<Customer> GetAllCustomers() {
         var sql = "SELECT * From Customers";
         var cmd = new SqlCommand(sql, sqlConnection);
+        var reader = cmd.ExecuteReader();
+        var customers = new List<Customer>();
+        while(reader.Read()) {
+            var cust = new Customer();
+            cust.Id = Convert.ToInt32(reader["Id"]);
+            cust.Name = Convert.ToString(reader["Name"]);
+            cust.City = Convert.ToString(reader["City"]);
+            cust.State = Convert.ToString(reader["State"]);
+            cust.Sales = Convert.ToDecimal(reader["Sales"]);
+            cust.Active = Convert.ToBoolean(reader["Active"]);
+            customers.Add(cust);
+        }
+        reader.Close();
+        return customers;
+    }
+
+    public List<Customer> GetCustomersByPartialName(string substr) {
+        var sql = "SELECT * From Customers " +
+                    " Where Name like '%'+@substr+'%' " +
+                    " Order by Sales desc; ";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        cmd.Parameters.AddWithValue("@substr", substr);
         var reader = cmd.ExecuteReader();
         var customers = new List<Customer>();
         while(reader.Read()) {
--- TestCustomerController/Program.cs
+++ TestCustomerController/Program.cs
@@ -16,13 +16,32 @@ if(conn.State != System.Data.ConnectionState.Open) {
 
 var custCtrl = new CustomersController(conn);
 
-//Customer? cust = custCtrl.GetCustomerById(10);
+//var newCust = new Customer {
+//    Id = 0, Name = "Acme Mfg", City = "Mason", State = "OH", Sales = 0, Active = true
+//};
+//custCtrl.InsertCustomer(newCust);
+
+
+//custCtrl.DeleteCustomer(41);
+//Customer? cust = custCtrl.GetCustomerById(41);
+//if(cust == null) {
+//    Console.WriteLine("Customer with id of 41 is not found!");
+//} else {
+//    Console.WriteLine(cust);
+//}
+
+//custCtrl.UpdateCustomer(cust);
+//cust = custCtrl.GetCustomerById(10);
 //Console.WriteLine(cust);
 
 //List<Customer> customers = custCtrl.GetAllCustomers();
 //foreach(var c in customers) {
 //    Console.WriteLine(c);
-//}
+//
+List<Customer> customers = custCtrl.GetCustomersByPartialName("er");
+foreach(var c in customers) {
+    Console.WriteLine(c);
+}
 
 // No code after here ***********************************
 conn.Close();
