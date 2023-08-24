d86b7362020f305164959f4c1d63938209e5421c
Greg Doud <gpdoud@gmail.com>
2023-08-24 14:23:09 -0400

    Add project files.

---
+++ CustomersControllerProject/CustomersController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Data.SqlClient;
+
+using System.Data.SqlTypes;
+
+namespace CustomersControllerProject;
+
+public class CustomersController {
+
+    public SqlConnection sqlConnection { get; set; }
+
+    public CustomersController(SqlConnection sqlConnection) {
+        this.sqlConnection = sqlConnection;
+    }
+
+    public Customer? GetCustomerById(int id) {
+        if(id <= 0) {
+            throw new ArgumentException("'id' must be greater than zero");
+        }
+        var sql = "SELECT * From Customers Where Id = @Id";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", id);
+        var reader = cmd.ExecuteReader();
+        if(!reader.HasRows) {
+            reader.Close();
+            return null;
+        }
+        reader.Read();
+        var cust = new Customer();
+        cust.Id = Convert.ToInt32(reader["Id"]);
+        cust.Name = Convert.ToString(reader["Name"]);
+        cust.City = Convert.ToString(reader["City"]);
+        cust.State = Convert.ToString(reader["State"]);
+        cust.Sales = Convert.ToDecimal(reader["Sales"]);
+        cust.Active = Convert.ToBoolean(reader["Active"]);
+        reader.Close();
+        return cust;
+    }
+
+    public List<Customer> GetAllCustomers() {
+        var sql = "SELECT * From Customers";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        var reader = cmd.ExecuteReader();
+        var customers = new List<Customer>();
+        while(reader.Read()) {
+            var cust = new Customer();
+            cust.Id = Convert.ToInt32(reader["Id"]);
+            cust.Name = Convert.ToString(reader["Name"]);
+            cust.City = Convert.ToString(reader["City"]);
+            cust.State = Convert.ToString(reader["State"]);
+            cust.Sales = Convert.ToDecimal(reader["Sales"]);
+            cust.Active = Convert.ToBoolean(reader["Active"]);
+            customers.Add(cust);
+        }
+        reader.Close();
+        return customers;
+    }
+}
---
+++ TestCustomerController/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using CustomersControllerProject;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlTypes;
+
+var connStr = "server=localhost\\sqlexpress;" +
+                "database=SalesDb;" +
+                "trusted_connection=true;" +
+                "trustServerCertificate=true;";
+var conn = new SqlConnection(connStr);
+conn.Open();
+if(conn.State != System.Data.ConnectionState.Open) {
+    throw new Exception("Connection did not open!!!");
+}
+// Code goes here ***************************************
+
+var custCtrl = new CustomersController(conn);
+
+//Customer? cust = custCtrl.GetCustomerById(10);
+//Console.WriteLine(cust);
+
+//List<Customer> customers = custCtrl.GetAllCustomers();
+//foreach(var c in customers) {
+//    Console.WriteLine(c);
+//}
+
+// No code after here ***********************************
+conn.Close();
